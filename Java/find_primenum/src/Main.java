7338a00aaae50f5c765cd8bfee8a1f22162fae34
beratpaban <beratpaban@gmail.com>
2023-09-25 23:17:28 +0300

    Find prime num

---
+++ Java/find_primenum/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int number=100;
+        for (int i = 2; i < number; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if(i%j==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+        }
+    }
+}

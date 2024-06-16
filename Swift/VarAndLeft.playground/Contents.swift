532ca8bcaef11df61b566fa7175be3eb07cdd9a1
Berat Paban <67541132+beratpaban@users.noreply.github.com>
2024-06-16 21:06:35 +0300

    swift first lesson

---
+++ Swift/VarAndLeft.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+var greeting = "Hello, "
+
+let pi = 3.14
+
+let pi2: Float = 3.14
+let isim: String = "Berat"
+
+print(greeting+isim)
+
+print("\(isim) Hello") 
+
+var ages = [3, 5, 7, 11, 22,23,24]
+var names = ["Berat","Mehmet","İbrahim"]
+
+print("\(names[0]), \(ages[4]) yasindadir.")
